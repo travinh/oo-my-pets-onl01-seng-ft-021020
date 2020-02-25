2edddd0a95fee0c0e2e24a85d7f03b92a5ff5dee
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-25 09:52:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
   # code goes here
+  
+  attr_reader :name, :species
+  
+  @@all=[]
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats 
+    cats = Cat.all.collect do |cat|
+      cat.owner == self
+    end
+  end
+  
+  
 end
